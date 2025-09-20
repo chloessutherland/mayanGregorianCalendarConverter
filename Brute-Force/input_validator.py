7735d416df5889b6b27c3385d8191a183c58d18e
chloessutherland <cascadingstylesheets@outlook.com>
2025-09-20 09:29:32 -0400

    Update input_validator.py
    More comments

--- Brute-Force/input_validator.py
+++ Brute-Force/input_validator.py
@@ -11,6 +11,7 @@ def is_correct_format(date_format, unformatted_input):
 
 # Check if the month is valid
 def is_valid_month(date_list):
+    # Must be between 1 and 12
     if date_list[0] > 12 or date_list[0] < 1:
         return False
     else:
@@ -25,10 +26,13 @@ def is_not_zero(date_list):
         return True
         
 
-# Check if the date is before August 11, 3114 BC
+# Check if the date is before August 12, 3114 BC
 def is_not_before_start(date_list):
+    # If year is before
     if date_list[2] < -3114 or (
+            # If year is 3114 BC and before August
             date_list[2] == -3114 and date_list[0] < 8
+            # If year is 3114 BC and before August and before the 12th
             or date_list[2] == -3114 and date_list[0] == 8 and date_list[1] < 12
         ):
         return False
@@ -38,6 +42,7 @@ def is_not_before_start(date_list):
         
 # Check if the day is a valid day of the month
 def is_valid_day(date_list, input_date):
+    # If the day is greater than the number of allowed days in the appropriate month in months_of_the_year
     if date_list[1] > input_date.months_of_the_year[date_list[0] - 1] or (
         date_list[1] < 1
         ):  # minus 1 because the list starts at index 0
