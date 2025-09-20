d3d3006eb9322a596fbbdd73e33b50268b593251
chloessutherland <cascadingstylesheets@outlook.com>
2025-09-20 09:23:07 -0400

    Update format_date.py
    More comments

--- Brute-Force/format_date.py
+++ Brute-Force/format_date.py
@@ -1,17 +1,19 @@
 from date import *
 
 
-# Create a string that formats a GregorianDate object into a human-readable date
+# Create a string that formats a GregorianDate object into a human-readable date: ##/##/####
 def format_gregorian(gregorian):
     format_string = str(gregorian.month) + "/" + str(gregorian.day) + "/"
+    # Format for BC
     if gregorian.year < 0:
         format_string += str(abs(gregorian.year)) + " BC"
+    # Format for AD
     else:
         format_string += str(gregorian.year)
     return format_string
 
 
-# Create a string that formats a MayaDate object into a human-readable date
+# Create a string that formats a MayaDate object into a human-readable date: [baktuns].[katuns].[tuns].[winals].[kins]
 def format_mayan(mayan):
     return (
         str(mayan.baktuns)
